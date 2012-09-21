4ba696de0643f00571964a2e677772983102d002
Claudio Scordino <claudio@evidence.eu.com>
2012-09-21 15:49:28 +0200

    Usage of dma_alloc_coherent

--- mmap_alloc.c
+++ mmap_alloc.c
@@ -14,9 +14,11 @@
 
 /*
  * Example of driver that allows a user-space program to mmap a buffer of
- * contiguous physical memory.
+ * contiguous non-cached physical memory.
  * Note that pages are contiguous in physical memory but not in virtual memory.
  * Based on the original work made by Martin Frey <frey@scs.ch>.
+ *
+ * Authors: Claudio Scordino, Bruno Morelli
  */
 
 /* character device structures */
@@ -42,6 +44,7 @@ static struct file_operations mmap_fops = {
 static int *alloc_area;
 // original pointer for allocated area
 static void *alloc_ptr;
+dma_addr_t dma_handle;
 
 /* character device open method */
 static int mmap_open(struct inode *inode, struct file *filp)
@@ -66,15 +69,27 @@ int mmap_kmem(struct file *filp, struct vm_area_struct *vma)
            pages allocated */
         if (length > NPAGES * PAGE_SIZE)
                 return -EIO;
-
+/* #ifdef ARCH_HAS_DMA_MMAP_COHERENT */
+	if (vma->vm_pgoff == 0) {
+		printk(KERN_INFO "Using dma_mmap_coherent\n");
+		ret = dma_mmap_coherent(NULL, vma, alloc_ptr,
+					dma_handle, length);
+	} else
+/* #else */
+	{
+		printk(KERN_INFO "Using remap_pfn_range\n");
+		vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
+		vma->vm_flags |= VM_IO;
+		printk(KERN_INFO "off=%d\n", vma->vm_pgoff);
+	        ret = remap_pfn_range(vma, vma->vm_start,
+			      PFN_DOWN(virt_to_phys(bus_to_virt(dma_handle))) +
+			      vma->vm_pgoff, length, vma->vm_page_prot);
+	}
+/* #endif */
         /* map the whole physically contiguous area in one piece */
-        if ((ret = remap_pfn_range(vma,
-	    vma->vm_start,
-	    virt_to_phys((void *)alloc_area) >> PAGE_SHIFT,
-	    length,
-	    vma->vm_page_prot)) < 0) {
-		printk(KERN_ERR "mmap_alloc: remap_pfn_range failed\n");
-                return ret;
+        if (ret < 0) {
+		printk(KERN_ERR "mmap_alloc: remap failed (%d)\n", ret);
+		return ret;
         }
         
         return 0;
@@ -93,32 +108,23 @@ static int __init mmap_alloc_init(void)
         int ret = 0;
         int i;
 
-        /* Allocate a memory area with kmalloc.
-	 * Will be rounded up to a page boundary.
-	 * Normal allocation: may sleep.
-	 */
-        if ((alloc_ptr = kmalloc((NPAGES + 2) * PAGE_SIZE,
-	    GFP_KERNEL|__GFP_DMA)) == NULL) {
+	/* Allocate not-cached memory area with dma_map_coherent. */
+	printk(KERN_INFO "Use dma_alloc_coherent\n");
+	alloc_ptr = dma_alloc_coherent (NULL, (NPAGES + 2) * PAGE_SIZE,
+					&dma_handle, GFP_KERNEL);
+
+	if (!alloc_ptr) {
                 printk(KERN_ERR
-		    "mmap_alloc: could not allocate memory with kmalloc\n");
+		    "mmap_alloc: dma_alloc_coherent error\n");
                 ret = -ENOMEM;
                 goto out;
         }
-
-        /* Not-cached memory area with dma_map_single. */
-	if (dma_map_single(NULL, alloc_ptr, (NPAGES + 2) * PAGE_SIZE,
-	    DMA_BIDIRECTIONAL) == 0) {
-                printk(KERN_ERR "mmap_alloc: could not set dma\n");
-                ret = -ENOMEM;
-                goto out_vfree;
-	}
-
 	printk(KERN_INFO "mmap_alloc: physical address is %lu\n",
-	    virt_to_phys(alloc_ptr));
+	    dma_handle);
+	printk(KERN_INFO "mmap_alloc: bus_to_virt %lu\n",
+	    virt_to_phys(bus_to_virt(dma_handle)));
 
-        /* round it up to the page bondary */
-        alloc_area = (int *)((((unsigned long)alloc_ptr) + PAGE_SIZE - 1) &
-	    PAGE_MASK);
+	alloc_area = alloc_ptr;
 
         /* get the major number of the character device */
         if ((ret = alloc_chrdev_region(&mmap_dev, 0, 1, "mmap_alloc")) < 0) {
@@ -127,8 +133,8 @@ static int __init mmap_alloc_init(void)
                 goto out_vfree;
         }
 
-        /* initialize the device structure and register the device with
-	 * the kernel */
+        /* initialize the device structure and register the device with the
+	 * kernel */
         cdev_init(&mmap_cdev, &mmap_fops);
         if ((ret = cdev_add(&mmap_cdev, mmap_dev, 1)) < 0) {
                 printk(KERN_ERR
@@ -136,13 +142,8 @@ static int __init mmap_alloc_init(void)
                 goto out_unalloc_region;
         }
 
-        /* mark the pages reserved */
-        for (i = 0; i < NPAGES * PAGE_SIZE; i+= PAGE_SIZE) {
-                SetPageReserved(virt_to_page(((unsigned long)alloc_area) + i));
-        }
-
-        /* Store a pattern in the memory.
-	 * The test application will check for it */
+	/* store a pattern in the memory.
+	 * the test application will check for it */
         for (i = 0; i < (NPAGES * PAGE_SIZE / sizeof(int)); i += 2) {
                 alloc_area[i] = (0xdead << 16) + i;
                 alloc_area[i + 1] = (0xbeef << 16) + i;
@@ -153,7 +154,8 @@ static int __init mmap_alloc_init(void)
   out_unalloc_region:
         unregister_chrdev_region(mmap_dev, 1);
   out_vfree:
-        kfree(alloc_ptr);
+	dma_free_coherent (NULL, (NPAGES + 2) * PAGE_SIZE, alloc_ptr,
+	    dma_handle);
   out:
         return ret;
 }
@@ -167,17 +169,14 @@ static void __exit mmap_alloc_exit(void)
         cdev_del(&mmap_cdev);
         unregister_chrdev_region(mmap_dev, 1);
 
-        /* unreserve the pages */
-        for (i = 0; i < NPAGES * PAGE_SIZE; i+= PAGE_SIZE) {
-                ClearPageReserved(virt_to_page(((unsigned long)alloc_area) + i));
-        }
-        /* free the memory areas */
-        kfree(alloc_ptr);
+	/* free the memory areas */
+	dma_free_coherent (NULL, (NPAGES + 2) * PAGE_SIZE, alloc_ptr,
+	    dma_handle);
 }
 
 module_init(mmap_alloc_init);
 module_exit(mmap_alloc_exit);
 MODULE_DESCRIPTION("mmap_alloc driver");
-MODULE_AUTHOR("Claudio Scordino");
+MODULE_AUTHOR("Claudio Scordino and Bruno Morelli");
 MODULE_LICENSE("GPL");
 
--- mmap_alloc_test.c
+++ mmap_alloc_test.c
@@ -8,19 +8,21 @@
 
 #define NPAGES 16
 
-/* Program to test mmap_alloc driver.
-   It reads out values from the buffer allocated by the driver and 
-   check the content correctness.
-
-   You need to manually create a device in dev/. To create it
-
-   1. Find the major number assigned to the driver:
-
-      grep mmap_alloc /proc/devices'
-
-   2. Create the special file (assuming major number 254)
-   
-      mknod /dev/mmap_alloc c 254 0
+/*
+ * Program to test mmap_alloc driver.
+ *
+ * It reads out values from the buffer allocated by the driver and
+ * check the content correctness.
+ *
+ * You need to manually create a device in dev/. To create it
+ *
+ * 1. Find the major number assigned to the driver
+ *
+ *	grep mmap_alloc /proc/devices'
+ *
+ * 2. Create the special file (assuming major number 254)
+ *
+ *	mknod /dev/mmap_alloc c 254 0
 */
 
 int main(void)
@@ -36,10 +38,10 @@ int main(void)
 	}
 	fprintf(stderr, "mmap_alloc: open OK\n");
 
-	kadr = mmap(0, len, PROT_READ|PROT_WRITE, MAP_SHARED| MAP_LOCKED, fd,
-	    len);
+	kadr = mmap(0, len, PROT_READ|PROT_WRITE, MAP_SHARED| MAP_LOCKED,
+	    fd, 0);
 
-	if (kadr == MAP_FAILED) {
+	if (kadr == MAP_FAILED)	{
 		perror("mmap");
 		exit(-1);
 	}
@@ -50,7 +52,7 @@ int main(void)
 	        (0xdead0000 + len / sizeof(int) - 2))
 	    || (kadr[len / sizeof(int) - 1] !=
 	        (0xbeef0000 + len / sizeof(int) - 2))) {
-		fprintf(stderr, "mmap_alloc: error\n");
+		fprintf(stderr, "mmap_alloc: check ERROR\n");
 		fprintf(stderr, "0x%x 0x%x\n", kadr[0], kadr[1]);
 		fprintf(stderr, "0x%x 0x%x\n", kadr[len / sizeof(int) - 2],
 		    kadr[len / sizeof(int) - 1]);
